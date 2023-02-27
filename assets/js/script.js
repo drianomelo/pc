78079e4a135d2a5752c7e009da0ed2c4e5a8a5a7
Adriano <adrianosenny@gmail.com>
2023-02-27 12:12:11 -0300

    Criando evento de click ao botao que faz a modal aparecer

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const btnModal = document.querySelector(".main__btn");
+const iconBtnModal = document.querySelector(".icon");
+const modal = document.querySelector(".modal");
+
+btnModal.addEventListener("click", () => {
+  console.log(btnModal.style.backgroundColor);
+  if (btnModal.style.backgroundColor === "rgb(96, 237, 119)") {
+    modal.style.opacity = "1";
+    modal.style.top = "15%";
+    btnModal.style.backgroundColor = "#ed6077";
+    iconBtnModal.style.transform = "rotate(45deg)";
+  } else {
+    modal.style.opacity = "0";
+    modal.style.top = "12%";
+    btnModal.style.backgroundColor = "#60ed77";
+    iconBtnModal.style.transform = "rotate(0)";
+  }
+});
