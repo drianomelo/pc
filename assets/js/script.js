4a1af95ad306a580a13562c56290552ef4e551a2
Adriano <adrianosenny@gmail.com>
2023-03-20 10:10:46 -0300

    Adicionando máscara de conversão para preço

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,78 +5,25 @@ const modal = document.querySelector(".modal");
 btnModal.addEventListener("click", () => {
   if (modal.style.opacity == "0") {
     modal.style.opacity = "1";
-    modal.style.top = "15%";
     btnModal.style.backgroundColor = "#ed6077";
     iconBtnModal.style.transform = "rotate(45deg)";
     modal.style.pointerEvents = "all";
   } else {
     modal.style.opacity = "0";
-    modal.style.top = "12%";
     btnModal.style.backgroundColor = "#60ed77";
     iconBtnModal.style.transform = "rotate(0)";
   }
 });
 
-//FUNÇAO PARA MASCARA DE PREÇO
+//MASCARA REAL
 
 function formatarPreco(input) {
-  // Limpa o valor digitado
   var valor = input.value.replace(/\D/g, "");
 
-  // Formata o valor de acordo com o padrão brasileiro
   valor = (valor / 100).toLocaleString("pt-BR", { minimumFractionDigits: 2 });
 
-  // Atualiza o valor no input
   input.value = valor;
 }
-
-// MODALS DE INPUT
-
-const modalPeca = document.querySelector(".modal-pecas");
-const modalPeriferico = document.querySelector(".modal-perifericos");
-const btnPeca = document.querySelector("#pecas");
-const btnPerifericos = document.querySelector("#perifericos");
-const btnAddPecas = document.querySelector(".btn-pecas");
-const btnAddPerifericos = document.querySelector(".btn-perifericos");
-const body = document.querySelector(".flex");
-
-btnPeca.addEventListener("click", () => {
-  modalPeca.style.display = "block";
-  body.style.filter = "brightness(0.4)";
-  modal.style.pointerEvents = "none";
-  modal.style.filter = "brightness(0.4)";
-});
-
-btnPerifericos.addEventListener("click", () => {
-  modalPeriferico.style.display = "block";
-  body.style.filter = "brightness(0.4)";
-  modal.style.pointerEvents = "none";
-  modal.style.filter = "brightness(0.4)";
-});
-
-btnAddPecas.addEventListener("click", () => {
-  modalPeriferico.style.display = "none";
-  modalPeca.style.display = "none";
-  body.style.filter = "brightness(1)";
-  modal.style.filter = "brightness(1)";
-  modal.style.pointerEvents = "all";
-});
-
-btnAddPerifericos.addEventListener("click", () => {
-  modalPeriferico.style.display = "none";
-  modalPeca.style.display = "none";
-  body.style.filter = "brightness(1)";
-  modal.style.filter = "brightness(1)";
-  modal.style.pointerEvents = "all";
-});
-
-body.addEventListener("click", () => {
-  modalPeriferico.style.display = "none";
-  modalPeca.style.display = "none";
-  body.style.filter = "brightness(1)";
-  modal.style.filter = "brightness(1)";
-  modal.style.pointerEvents = "all";
-});
 
 const formPecas = document.querySelector("#form-pecas");
 const itensPecas = JSON.parse(localStorage.getItem("list__item-pecas")) || [];
@@ -144,7 +91,10 @@ formPerifericos.addEventListener("submit", (evento) => {
 
   itensPerifericos.push(itemAtual);
 
-  localStorage.setItem("list__item-perifericos", JSON.stringify(itensPerifericos));
+  localStorage.setItem(
+    "list__item-perifericos",
+    JSON.stringify(itensPerifericos)
+  );
 
   nome.value = "";
 });
